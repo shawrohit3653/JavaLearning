0c1e5a4405aa30a34396caa99742bf8a6b116810
Rohit <rohit@Rahuls-MacBook-Pro.local>
2026-04-09 19:36:38 +0530

    more pattern

---
+++ Pattern/Square2.java
@@ -0,0 +1,13 @@
+package Pattern;
+
+public class Square2 {
+   public static void main() {
+        int n=3;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<5;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
